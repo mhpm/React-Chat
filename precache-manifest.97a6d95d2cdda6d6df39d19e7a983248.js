d804692d6fd68333e7724fc33bf263d6b5d89c35
michelle perez <mmorales@boomsourcing.com>
2020-04-08 14:30:26 -0600

    Updates

---
+++ precache-manifest.97a6d95d2cdda6d6df39d19e7a983248.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b10d712f8c5582fee7e0b248c6db5247",
+    "url": "/react-chat/index.html"
+  },
+  {
+    "revision": "dbdc5c70be75988f2324",
+    "url": "/react-chat/static/css/main.09593ef9.chunk.css"
+  },
+  {
+    "revision": "79bdfd0842635ff811f2",
+    "url": "/react-chat/static/js/2.34d6f88e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-chat/static/js/2.34d6f88e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbdc5c70be75988f2324",
+    "url": "/react-chat/static/js/main.2ebd2bba.chunk.js"
+  },
+  {
+    "revision": "dcec123971f273a932dd",
+    "url": "/react-chat/static/js/runtime-main.8945d9b2.js"
+  },
+  {
+    "revision": "023d1b62c481988e84fb48bd784f982a",
+    "url": "/react-chat/static/media/logo.023d1b62.png"
+  }
+]);
