940ea266a75c7509a67bbd8a68456890e004cbc9
michelle perez <mmorales@boomsourcing.com>
2020-04-08 20:20:54 -0600

    Updates

--- precache-manifest.6312f666fa5cdf931247ad74e5164895.js
+++ precache-manifest.6312f666fa5cdf931247ad74e5164895.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3aa71e120dac0c68189b8f7731665644",
+    "revision": "ced24d7dfcba3516576d3d784f32dbd9",
     "url": "/react-chat/index.html"
   },
   {
-    "revision": "afddaaca9d9e3e1f1b42",
+    "revision": "a54b3205256ebad81883",
     "url": "/react-chat/static/css/main.09593ef9.chunk.css"
   },
   {
-    "revision": "9f390c89a75519ae59e9",
-    "url": "/react-chat/static/js/2.fd07898e.chunk.js"
+    "revision": "d9f0f91f1d5b121bf5da",
+    "url": "/react-chat/static/js/2.b852fbe3.chunk.js"
   },
   {
     "revision": "b67bc97fd0dd67004287af4028afc1b1",
-    "url": "/react-chat/static/js/2.fd07898e.chunk.js.LICENSE.txt"
+    "url": "/react-chat/static/js/2.b852fbe3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "afddaaca9d9e3e1f1b42",
-    "url": "/react-chat/static/js/main.38bcdf08.chunk.js"
+    "revision": "a54b3205256ebad81883",
+    "url": "/react-chat/static/js/main.9e9a0d30.chunk.js"
   },
   {
     "revision": "dcec123971f273a932dd",
